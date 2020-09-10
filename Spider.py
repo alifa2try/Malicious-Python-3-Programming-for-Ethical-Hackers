22347a7f576d2f659f6090894d75c4a614575abf
Faisal Ali Garba <alifa2try@gmail.com>
2020-09-10 23:46:32 +0100

    Add files via upload

---
+++ Spider.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# This program crawls websites
+
+
+# import modules
+import argparse
+import requests
+import re
+import urllib.parse
+
+
+def get_argument():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("domain", help="Domain to search email from")
+	option = parser.parse_args()
+
+	if not option.domain:
+		parser.error("[-] You need to specify a domain to search emails from")
+
+	return option.domain	
+
+
+def get_links_from_url(url):
+	try:
+		response = requests.get(url)
+		return re.findall('(?:href=")(.*?)"', (response.content).decode())
+	except UnicodeDecodeError:
+		pass	
+	
+	
+
+def crawl_links(url):
+	try:
+		links = get_links_from_url(url)
+
+		for link in links:
+			link = urllib.parse.urljoin(url, link)
+			
+			if '#' in link:
+				link = link.split('#')[0]
+				
+
+			if target_url in link and link not in link_list:
+				link_list.append(link)
+				print(f"[+] Found a link >> {link}")
+				crawl_links(link)
+	except TypeError:
+		pass			
+
+try:
+	link_list = []
+	domain = get_argument()
+	target_url = "https://" + domain
+	crawl_links(target_url)
+except KeyboardInterrupt:
+	print("[+] Detected CTRL + C and the program will now halt!")
